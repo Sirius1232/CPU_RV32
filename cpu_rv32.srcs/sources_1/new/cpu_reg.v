b30f0d8d3542a611f3be3d63e7449564725f2a83
ZhengPW <1623905151@qq.com>
2023-07-10 20:57:54 +0800

    v3.2.2: Added some comments.

--- cpu_rv32.srcs/sources_1/new/cpu_reg.v
+++ cpu_rv32.srcs/sources_1/new/cpu_reg.v
@@ -2,9 +2,12 @@
 // Encoding:            UTF-8
 //----------------------------------------------------------------------------------------
 // File Name:           cpu_reg.v
-// Descriptions:        通用寄存器管理模块
+// Descriptions:        通用寄存器管理模块（写回模块）
 //-----------------------------------------README-----------------------------------------
+// 实现对通用寄存器的读写控制。
 // 
+// - 读：简单的组合逻辑电路，根据输入的寄存器地址引出相应的数据即可，不需要时序控制；
+// - 写：每一个时钟周期完成一次写操作
 // 
 //----------------------------------------------------------------------------------------
 //****************************************************************************************//
@@ -13,14 +16,14 @@ module cpu_reg (
         input               clk,
         input               rst_n,
         input       [4:0]   rs1,
-        input       [4:0]   rs2,
-        input       [4:0]   jmp_rs,
-        input       [4:0]   rd,
-        input               wr_en,
-        input       [31:0]  data_rd,
         output      [31:0]  data1,
+        input       [4:0]   rs2,
         output      [31:0]  data2,
-        output      [31:0]  data_jmp_rs
+        input       [4:0]   jmp_rs,
+        output      [31:0]  data_jmp_rs,
+        input               wr_en,  // 写使能
+        input       [4:0]   rd,  // 写寄存器地址
+        input       [31:0]  data_rd  // 写数据
     );
 
     integer i;
@@ -41,7 +44,7 @@ module cpu_reg (
             end
         end
         else if(wr_en) begin
-            if(rd==0)
+            if(rd==0)  // x0寄存器为硬件零
                 register[0] <= 32'd0;
             else
                 register[rd] <= data_rd;
